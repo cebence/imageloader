04f7f4c7d5444b8743e18fec47535b5250ca00d6
cebence <dejan.cebetarevic@gmail.com>
2014-11-28 13:19:39 +0100

    Add some automation to the tests.

---
+++ gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    qunit: {
+      all: 'test/**/*.html'
+    }
+  });
+
+  // Load task plugins.
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+
+  // Register default task(s).
+  grunt.registerTask('default', [ 'qunit' ]);
+};
