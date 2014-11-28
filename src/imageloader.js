20dc5f6f23601742357ae5b10d977225bc000303
Dejan Ćebetarević <dejan.cebetarevic@gmail.com>
2014-11-28 12:43:07 +0100

    Initial release of ImageLoader.

---
+++ src/imageloader.js
@@ -0,0 +1,244 @@
+;(function() {
+  var jqueryFound = typeof jQuery !== 'undefined',
+      nativeBind = Function.prototype.bind,
+      nativeEvents = !!Image.prototype.addEventListener;
+
+  // Use jQuery.extend() or borrow from L.Util.extend().
+  var extend = jqueryFound ? jQuery.extend : function(dest) {
+        var i, j, len, src;
+        for (j = 1, len = arguments.length; j < len; j++) {
+          src = arguments[j];
+          for (i in src) {
+            dest[i] = src[i];
+          }
+        }
+        return dest;
+      },
+      bind = function(fn, obj) {
+        var slice = Array.prototype.slice;
+
+        // Use native Function.bind() or borrow from L.Util.bind().
+        if (nativeBind && fn.bind === nativeBind) {
+          return nativeBind.apply(fn, slice.call(arguments, 1));
+        }
+
+        var args = slice.call(arguments, 2);
+        return function() {
+          return fn.apply(obj, args.length ? args.concat(slice.call(arguments)) : arguments);
+        };
+      },
+      isArray = Array.isArray || function(obj) {
+        return (Object.prototype.toString.call(obj) === '[object Array]');
+      };
+
+  // Use native events where possible.
+  var addListener = function(obj, event, fn) {
+        if (nativeEvents) {
+          obj.addEventListener(event, fn, false);
+        }
+        else {
+          obj['on' + event] = fn;
+        }
+      },
+      removeListener = function(obj, event, fn) {
+        if (nativeEvents) {
+          obj.removeEventListener(event, fn);
+        }
+        else {
+          obj['on' + event] = null;
+        }
+      };
+
+  var defaultOptions = {
+    autoStart: true,
+    parallel: true,
+    onComplete: function() {}
+  };
+
+  /**
+   * Loading status constants.
+   */
+  var NOT_LOADED = 'not-loaded',
+      LOADING = 'loading',
+      LOADED = 'loaded';
+
+  /**
+   * Event-driven customizable JavaScript image pre-/re-/loader.
+   * Once created it can be used for reloading.
+   *
+   * @param { array | string } urls One or more URLs to load.
+   * @param { object } options Options to use when loading and/or creating images.
+   * Defaults to `{ autoStart: true, parallel: true }`.
+   */
+  var ImageLoader = function(urls, options) {
+    // Bind event listeners.
+    this._onLoadBound = bind(this._onLoad, this);
+    this._onErrorBound = bind(this._onError, this);
+    this._onAbortBound = bind(this._onAbort, this);
+
+    // Set initial options.
+    this._options = extend(defaultOptions, options);
+
+    // Set URLs to load.
+    this.setUrls(urls);
+
+    // Start loading automatically if required and there's something to load.
+    if (this._options.autoStart && this._urls.length) {
+      this.load();
+    }
+  };
+
+  // Make the loading status constants public.
+  ImageLoader.NOT_LOADED = NOT_LOADED;
+  ImageLoader.LOADING = LOADING;
+  ImageLoader.LOADED = LOADED;
+
+  /**
+   * Sets the image loader options, overrides existing ones (shallow copy).
+   *
+   * @param { object } options Options to set/modify.
+   * @return { object ImageLoader }
+   */
+  ImageLoader.prototype.setOptions = function(options) {
+    extend(this._options, options);
+    return this;
+  };
+
+  /**
+   * Stores the list of URLs to load (a local copy is made).
+   *
+   * @param { array | string } urls One or more URLs to load.
+   * @return { object ImageLoader }
+   */
+  ImageLoader.prototype.setUrls = function(urls) {
+    this._urls = [];
+    if (typeof urls !== 'undefined') {
+      this._urls = isArray(urls) ? urls.slice() : [ urls ];
+    }
+
+    return this;
+  };
+
+  /**
+   * Creates a new `HTMLImageElement` .
+   *
+   * @param { number } index Index of the image.
+   * @return { HTMLImageElement }
+   */
+  ImageLoader.prototype.createImage = function(index) {
+    var img = new Image();
+    img.dataset.loaderStatus = NOT_LOADED;
+    return img;
+  };
+
+  /**
+   * Adds itself as listener to image's events.
+   *
+   * @private
+   */
+  ImageLoader.prototype._attachListeners = function(image) {
+    addListener(image, 'load', this._onLoadBound);
+    addListener(image, 'error', this._onErrorBound);
+    addListener(image, 'abort', this._onAbortBound);
+  };
+
+  /**
+   * Removes itself as listener to image's events.
+   *
+   * @private
+   */
+  ImageLoader.prototype._detachListeners = function(image) {
+    removeListener(image, 'load', this._onLoadBound);
+    removeListener(image, 'error', this._onErrorBound);
+    removeListener(image, 'abort', this._onAbortBound);
+  };
+
+  /**
+   * `img.onload` event listener
+   *
+   * @private
+   */
+  ImageLoader.prototype._onLoad = function(event) {
+console.log('img.onload: ' + event.target.src);//XXX
+    var img = event.target;
+    img.dataset.loaderStatus = LOADED;
+    this._detachListeners(img);
+  };
+
+  /**
+   * `img.onerror` event listener
+   *
+   * @private
+   */
+  ImageLoader.prototype._onError = function(event) {
+    var img = event.target;
+    img.dataset.loaderStatus = NOT_LOADED;
+    this._detachListeners(img);
+  };
+
+  /**
+   * `img.onabort` event listener
+   *
+   * @private
+   */
+  ImageLoader.prototype._onAbort = function(event) {
+    var img = event.target;
+    img.dataset.loaderStatus = NOT_LOADED;
+    this._detachListeners(img);
+  };
+
+  /**
+   * Returns (a copy of) the array of all images as-is, i.e. whatever the status.
+   *
+   * @return { array:HTMLImageElement }
+   */
+  ImageLoader.prototype.getImages = function() {
+    return this._images ? this._images.slice() : [];
+  };
+
+  /**
+   * Starts loading images. Called automatically if `autoStart` option is `true`.
+   */
+  ImageLoader.prototype.load = function() {
+    var i,
+        opts = this._options,
+        urls = this._urls;
+
+    this._images = this._images || [];
+
+    if (opts.parallel) {
+      for (i = 0; i < urls.length; i++) {
+        this._loadImage(urls[i], i);
+      }
+    }
+    else {
+      this._loadImage(urls[0], 0);
+    }
+
+    return this;
+  };
+
+  /**
+   * Single image loading.
+   *
+   * @private
+   */
+  ImageLoader.prototype._loadImage = function(url, index) {
+    var img = this.createImage(index);
+    this._images[index] = img;
+    img.dataset.loaderStatus = LOADING;
+    this._attachListeners(img);
+    img.src = url;
+  };
+
+
+  // Define the module.
+  if (typeof define === 'function' && define.amd) {
+    define(function() {
+      return ImageLoader;
+    });
+  }
+  else {
+    this.ImageLoader = ImageLoader;
+  }
+})();
