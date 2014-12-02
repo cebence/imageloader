d5f0c1417036def1c8f738774b5ebbf29baced2a
cebence <dejan.cebetarevic@gmail.com>
2014-12-02 16:21:35 +0100

    Fix tests for parallel and sequential loading.

--- test/tests.js
+++ test/tests.js
@@ -7,6 +7,35 @@ var INVALID_IMAGE = "data:image/gif;base64,invalid=",
     WHITE_GIF = "data:image/gif;base64,R0lGODdhAQABAIAAAP///////ywAAAAAAQABAAACAkQBADs=",
     VALID_IMAGES = [ BLACK_GIF, RED_GIF, WHITE_GIF];
 
+/**
+ * Functions for building URLs for dummy images (large & small).
+ * TODO Refactor into separate ".js" file.
+ */
+function randomColor() {
+  var r = Math.round(255 * Math.random()),
+      g = Math.round(255 * Math.random()),
+      b = Math.round(255 * Math.random());
+  return r.toString(16) + g.toString(16) + b.toString(16);
+}
+
+/**
+ * Returns a URL to a PNG image in HD resolution (1280 x 720).
+ */
+function dummyImageLarge() {
+  return 'http://dummyimage.com/1280x720/'
+      + randomColor() + '/' + randomColor() + '.png?' + Date.now();
+}
+
+/**
+ * Returns a URL to a GIF image 50 x 50 pixels.
+ * A GIF image is generally smaller (in bytes) than equivalent PNG image.
+ */
+function dummyImageSmall() {
+  return 'http://dummyimage.com/50x50/'
+      + randomColor() + '/' + randomColor()  + '.gif?' + Date.now();
+}
+
+
 QUnit.asyncTest("Nothing to load (no args)", function (assert) {
   var loader = new ImageLoader();
   assert.ok(loader instanceof ImageLoader, "ImageLoader instance created.");
@@ -17,7 +46,7 @@ QUnit.asyncTest("Nothing to load (no args)", function (assert) {
     assert.equal(actual, expected, "There was nothing to load.");
 
     QUnit.start();
-  }, 5);
+  }, 10);
 });
 
 QUnit.asyncTest("Load single URL (not an array)", function (assert) {
@@ -30,7 +59,7 @@ QUnit.asyncTest("Load single URL (not an array)", function (assert) {
     assert.equal(actual, expected, "Single image loaded.");
 
     QUnit.start();
-  }, 5);
+  }, 10);
 });
 
 QUnit.asyncTest("Load multiple URLs", function (assert) {
@@ -43,7 +72,7 @@ QUnit.asyncTest("Load multiple URLs", function (assert) {
     assert.equal(actual, expected, "Multiple images loaded.");
 
     QUnit.start();
-  }, 5);
+  }, 10);
 });
 
 QUnit.asyncTest("Load a valid image", function (assert) {
@@ -58,7 +87,7 @@ QUnit.asyncTest("Load a valid image", function (assert) {
     assert.equal(actual[0].dataset.loaderStatus, ImageLoader.LOADED, "Image loaded.");
 
     QUnit.start();
-  }, 5);
+  }, 10);
 });
 
 QUnit.asyncTest("Load an invalid image", function (assert) {
@@ -73,7 +102,7 @@ QUnit.asyncTest("Load an invalid image", function (assert) {
     assert.equal(actual[0].dataset.loaderStatus, ImageLoader.NOT_LOADED, "Image not loaded.");
 
     QUnit.start();
-  }, 5);
+  }, 10);
 });
 
 QUnit.asyncTest("Confirm loaded URLs", function (assert) {
@@ -89,7 +118,7 @@ QUnit.asyncTest("Confirm loaded URLs", function (assert) {
     }
 
     QUnit.start();
-  }, 5);
+  }, 10);
 });
 
 QUnit.asyncTest("onProgress fired for a valid image", function (assert) {
@@ -107,7 +136,7 @@ QUnit.asyncTest("onProgress fired for a valid image", function (assert) {
     assert.equal(event.failed, 0, "event.failed confirmed.");
 
     QUnit.start();
-  }, 5);
+  }, 10);
 });
 
 QUnit.asyncTest("onProgress fired for an invalid image", function (assert) {
@@ -118,6 +147,7 @@ QUnit.asyncTest("onProgress fired for an invalid image", function (assert) {
   setTimeout(function() {
     assert.equal(listener.callCount, 1, "onProgress fired once.");
     assert.ok(listener.calledWith(sinon.match.object), "Event object present.");
+
     var event = listener.firstCall.args[0];
     assert.equal(event.url, INVALID_IMAGE, "event.url confirmed.");
     assert.equal(event.status, ImageLoader.NOT_LOADED, "event.status confirmed.");
@@ -125,7 +155,7 @@ QUnit.asyncTest("onProgress fired for an invalid image", function (assert) {
     assert.equal(event.failed, 1, "event.failed confirmed.");
 
     QUnit.start();
-  }, 5);
+  }, 10);
 });
 
 QUnit.asyncTest("onProgress fired for mulitple valid images", function (assert) {
@@ -138,7 +168,7 @@ QUnit.asyncTest("onProgress fired for mulitple valid images", function (assert) 
     assert.equal(listener.callCount, expected, "onProgress fired " + expected + " times.");
 
     QUnit.start();
-  }, 5);
+  }, 10);
 });
 
 QUnit.asyncTest("onComplete fired for one valid image", function (assert) {
@@ -149,6 +179,7 @@ QUnit.asyncTest("onComplete fired for one valid image", function (assert) {
   setTimeout(function() {
     assert.equal(listener.callCount, 1, "onComplete fired once.");
     assert.ok(listener.calledWith(sinon.match.object), "Event object present.");
+
     var event = listener.firstCall.args[0];
     assert.equal(event.loader, loader, "event.loader confirmed.");
     assert.equal(event.total, 1, "event.total confirmed.");
@@ -156,7 +187,7 @@ QUnit.asyncTest("onComplete fired for one valid image", function (assert) {
     assert.equal(event.failed, 0, "event.failed confirmed.");
 
     QUnit.start();
-  }, 5);
+  }, 10);
 });
 
 QUnit.asyncTest("onComplete fired for an invalid image", function (assert) {
@@ -167,6 +198,7 @@ QUnit.asyncTest("onComplete fired for an invalid image", function (assert) {
   setTimeout(function() {
     assert.equal(listener.callCount, 1, "onComplete fired once.");
     assert.ok(listener.calledWith(sinon.match.object), "Event object present.");
+
     var event = listener.firstCall.args[0];
     assert.equal(event.loader, loader, "event.loader confirmed.");
     assert.equal(event.total, 1, "event.total confirmed.");
@@ -174,7 +206,7 @@ QUnit.asyncTest("onComplete fired for an invalid image", function (assert) {
     assert.equal(event.failed, 1, "event.failed confirmed.");
 
     QUnit.start();
-  }, 5);
+  }, 10);
 });
 
 QUnit.asyncTest("onComplete fired for multiple mixed images", function (assert) {
@@ -186,6 +218,7 @@ QUnit.asyncTest("onComplete fired for multiple mixed images", function (assert) 
   setTimeout(function() {
     assert.equal(listener.callCount, 1, "onComplete fired once.");
     assert.ok(listener.calledWith(sinon.match.object), "Event object present.");
+
     var event = listener.firstCall.args[0];
     assert.equal(event.loader, loader, "event.loader confirmed.");
     assert.equal(event.total, 3, "event.total confirmed.");
@@ -193,7 +226,7 @@ QUnit.asyncTest("onComplete fired for multiple mixed images", function (assert) 
     assert.equal(event.failed, 1, "event.failed confirmed.");
 
     QUnit.start();
-  }, 5);
+  }, 10);
 });
 
 QUnit.asyncTest("onComplete fired last", function (assert) {
@@ -211,30 +244,45 @@ QUnit.asyncTest("onComplete fired last", function (assert) {
     assert.ok(onComplete.calledAfter(onProgress), "onComplete fired last.");
 
     QUnit.start();
-  }, 5);
+  }, 10);
 });
 
-QUnit.asyncTest("Serial Loading (in sequence, non-parallel)", function (assert) {
+QUnit.asyncTest("Parallel loading", function (assert) {
+  // Load one large image and one small to make sure the order of "onProgress" events.
+  var urls = [ dummyImageLarge(), dummyImageSmall() ],
+      onProgress = sinon.spy(),
+      continueTest = function() {
+        assert.equal(onProgress.callCount, 2, "onProgress fired 2 times.");
+        assert.equal(onProgress.getCall(0).args[0].index, 1, "Small image was first.");
+        assert.equal(onProgress.getCall(1).args[0].index, 0, "Large image was second.");
+
+        QUnit.start();
+      },
+      loader = new ImageLoader(urls, {
+        parallel: true,
+        onProgress: onProgress,
+        onComplete: continueTest
+      });
+});
+
+QUnit.asyncTest("Serial loading (in sequence)", function (assert) {
   var onProgress = sinon.spy(),
-      onComplete = sinon.spy(),
+      continueTest = function() {
+        var i, expected = VALID_IMAGES.length;
+        assert.equal(onProgress.callCount, expected, "onProgress fired " + expected + " times.");
+
+        for (i = 0; i < expected; i++) {
+          var event = onProgress.getCall(i).args[0];
+          assert.equal(event.url, VALID_IMAGES[i], "#" + (1 + i) + " event.url confirmed.");
+          assert.equal(event.count, (1 + i), "#" + (1 + i) + " count confirmed.");
+          assert.equal(event.index, i, "#" + (1 + i) + " index confirmed.");
+        }
+
+        QUnit.start();
+      },
       loader = new ImageLoader(VALID_IMAGES, {
         parallel: false,
         onProgress: onProgress,
-        onComplete: onComplete
+        onComplete: continueTest
       });
-  assert.ok(loader instanceof ImageLoader, "ImageLoader instance created.");
-
-  setTimeout(function() {
-    var i, expected = VALID_IMAGES.length;
-    assert.equal(onProgress.callCount, expected, "onProgress fired " + expected + " times.");
-    assert.equal(onComplete.callCount, 1, "onComplete fired once.");
-    assert.ok(onComplete.calledAfter(onProgress), "onComplete fired last.");
-
-    for (i = 0; i < onProgress.callCount; i++) {
-      var event = onProgress.getCall(i).args[0];
-      assert.equal(event.url, VALID_IMAGES[i], "#" + (1 + i) + " event.url confirmed.");
-    }
-
-    QUnit.start();
-  }, 5);
 });
